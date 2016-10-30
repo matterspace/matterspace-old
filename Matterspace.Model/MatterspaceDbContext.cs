00845df35cd491531ad13d62814b37f78d1e38eb
André Pena <andrerpena@gmail.com>
2016-10-30 16:21:01 -0200

    Setting up migrations for the first model

---
+++ Matterspace.Model/MatterspaceDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Matterspace.Model
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class MatterspaceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public MatterspaceDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MatterspaceDbContext Create()
+        {
+            return new MatterspaceDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogins");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityRole>().ToTable("UserRole");
+        }
+    }
+}
