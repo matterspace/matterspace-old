8e11ac6d0f42e7587d5e2918417f48036ab69929
andrerpena <andrerpena@gmail.com>
2016-11-07 00:14:24 -0200

    Starting to implement the dropdown menu for logged users

---
+++ Matterspace/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+
+    // will remove the active class from all nav items
+    $("body")
+        .click(function() {
+            $("ul.navbar-with-dropdown-menu li.nav-item").removeClass("active");
+        });
+
+    // will toggle the active class from nav items
+    $("ul.navbar-with-dropdown-menu li.nav-item")
+        .click(function (e) {
+            $(this).toggleClass("active");
+            e.stopPropagation();
+        });
+});
