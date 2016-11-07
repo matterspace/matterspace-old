0d8c3b9f62fbe0e85d79910375f9110da09bd60d
andrerpena <andrerpena@gmail.com>
2016-11-07 00:28:23 -0200

    Adding tooltip support for the navbar

--- Matterspace/Scripts/app.js
+++ Matterspace/Scripts/app.js
@@ -1,15 +1,31 @@
 ﻿$(function () {
 
+    // enable tooltips everywhere
+    $('[data-toggle="tooltip"]')
+        .tooltip({
+            trigger: "hover"
+        })
+        .on("show.bs.tooltip",
+            function () {
+                // tooltips should not be displayed when there's an active li.nav-item
+                if ($("ul.navbar-with-dropdown-menu li.nav-item.active").length)
+                    return false;
+            });
+
     // will remove the active class from all nav items
     $("body")
-        .click(function() {
+        .click(function () {
             $("ul.navbar-with-dropdown-menu li.nav-item").removeClass("active");
         });
 
     // will toggle the active class from nav items
     $("ul.navbar-with-dropdown-menu li.nav-item")
         .click(function (e) {
+            // hides the tooltip
+            $('[data-toggle="tooltip"]').tooltip("hide");
+            // toggles the active class. This will show or display the dropdown menu
             $(this).toggleClass("active");
             e.stopPropagation();
         });
+
 });
