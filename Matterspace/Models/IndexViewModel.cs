479eb676909cd2abe4264e3971392399832bc0b4
andrerpena <andrerpena@gmail.com>
2016-11-15 17:52:00 -0200

    Applying SASS!!!

--- Matterspace/Models/IndexViewModel.cs
+++ Matterspace/Models/IndexViewModel.cs
@@ -5,12 +5,20 @@ namespace Matterspace.Models
 {
     public class IndexViewModel
     {
+        public IndexViewModel()
+        {
+            this.Updates = new List<TimelineUpdateViewModel>();
+        }
+
+        /// <summary>
+        /// User time-line updates
+        /// </summary>
+        public IList<TimelineUpdateViewModel> Updates { get; set; }
+
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
-
-
     }
 }
