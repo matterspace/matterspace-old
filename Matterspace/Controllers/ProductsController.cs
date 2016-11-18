aa2746ce81869413449b72571da34b4b47ae3c06
andrerpena <andrerpena@gmail.com>
2016-11-18 21:52:24 -0200

    Getting started with the new product form

---
+++ Matterspace/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Matterspace.Models;
+
+namespace Matterspace.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Create()
+        {
+            var viewModel = new CreateProductViewModel();
+            return View(viewModel);
+        }
+    }
+}
