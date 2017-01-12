1d2274f5fef12af1a4880ab57ed1a57325541532
andrerpena <andrerpena@gmail.com>
2017-01-12 21:10:18 -0200

    Final version of the MarkdownEditor

---
+++ Statics/MarkdownEditor/demo/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { ReactMde, ReactMdeCommands } from 'react-mde';
+
+class App extends Component {
+
+    state = {
+        mdeValue: {text: "", selection: null}
+    }
+
+    handleValueChange(value) {
+        this.setState({mdeValue: value});
+    }
+
+    render() {
+        // get the default commands, you can pick individual commands if you like, or add your own
+        let commands = ReactMdeCommands.getDefaultCommands()
+        return (
+            <div className="container">
+                <ReactMde
+                    textareaId="ta1"
+                    textareaName="ta1"
+                    value={this.state.mdeValue}
+                    onChange={this.handleValueChange.bind(this)}
+                    commands={commands} />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ Statics/MarkdownEditor/demo/client.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App';
+
+
+// stylings
+    import 'normalize.css/normalize.css';
+    import 'font-awesome/css/font-awesome.css';
+    import 'react-mde/lib/styles/react-mde.scss';
+    import 'react-mde/lib/styles/react-mde-command-styles.scss';
+    import 'react-mde/lib/styles/markdown-default-theme.scss';
+    import './styles/demo.scss';
+
+render(
+    <App>
+    </App>,
+    document.getElementById('#app_container')
+);
