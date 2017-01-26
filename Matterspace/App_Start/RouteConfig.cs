195ffbf79a0f199b53a17d96f29e4723cd7c7b9d
Thiago Silva <thiagodd.silva@gmail.com>
2017-01-26 20:49:43 -0200

    fix bug #11

--- Matterspace/App_Start/RouteConfig.cs
+++ Matterspace/App_Start/RouteConfig.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Matterspace
 {
     public class RouteConfig
     {
+        public static string PRODUCT_HOME = "Product home";
+        public static string PRODUCT_ACTIONS = "Product's actions";
+        public static string PRODUCT = "Product";
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Product home",
+                name: PRODUCT_HOME,
                 url: "p/{productName}",
                 defaults: new { controller = "Products", action = "Index" }
             );
 
             routes.MapRoute(
-                name: "Product's actions",
+                name: PRODUCT_ACTIONS,
                 url: "p/{productName}/{controller}/{action}/{id}",
                 defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
-                name: "Product",
+                name: PRODUCT,
                 url: "p/{productName}/{action}",
                 defaults: new { controller = "Products" }
             );            
