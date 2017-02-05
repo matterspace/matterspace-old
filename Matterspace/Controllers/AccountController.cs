5bfcc403e767b1410c7a759fd7016d5b576274d4
Thiago Silva <thiagodd.silva@gmail.com>
2017-02-05 14:28:42 -0200

    send email with templates

--- Matterspace/Controllers/AccountController.cs
+++ Matterspace/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Matterspace.Models;
+using Matterspace.Lib.Email;
 
 namespace Matterspace.Controllers
 {
@@ -163,7 +164,8 @@ namespace Matterspace.Controllers
                     // Send an email with this link
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", EmailApi.CreateAccountConfirmationBody(user.UserName, callbackUrl, "Temporary body"));
 
                     return RedirectToAction("Index", "Home");
                 }
--- Matterspace/Lib/Email/EmailApi.cs
+++ Matterspace/Lib/Email/EmailApi.cs
@@ -1,6 +1,7 @@
 ﻿using RestSharp;
 using RestSharp.Authenticators;
 using System;
+using System.IO;
 using System.Web.Configuration;
 
 namespace Matterspace.Lib.Email
@@ -13,13 +14,9 @@ namespace Matterspace.Lib.Email
         /// <param name="message"></param>
         public static void SendEmail(EmailMessage message)
         {
-            var client = new RestClient
-            {
-                BaseUrl = new Uri(WebConfigurationManager.AppSettings[AppSettingsConstants.MailGunApi]),
-                Authenticator =
-                    new HttpBasicAuthenticator("api",
-                        WebConfigurationManager.AppSettings[AppSettingsConstants.MailGunApiSecrectKey])
-            };
+            var client = new RestClient();
+            client.BaseUrl = new Uri(WebConfigurationManager.AppSettings[AppSettingsConstants.MailGunApi]);
+            client.Authenticator = new HttpBasicAuthenticator("api", WebConfigurationManager.AppSettings[AppSettingsConstants.MailGunApiSecrectKey]);
 
             var request = new RestRequest();
             request.AddParameter("domain", WebConfigurationManager.AppSettings[AppSettingsConstants.MailGunDomain], ParameterType.UrlSegment);
@@ -27,10 +24,29 @@ namespace Matterspace.Lib.Email
             request.AddParameter("from", WebConfigurationManager.AppSettings[AppSettingsConstants.MailGunFrom]);
             request.AddParameter("to", message.Destination);
             request.AddParameter("subject", message.Subject);
-            request.AddParameter("text", message.Body);
+            request.AddParameter("html", message.Body);
 
             request.Method = Method.POST;
-            client.Execute(request);
+            var response = client.Execute(request);
+        }
+
+        /// <summary>
+        /// Creates a body for a email with account confirmation link. A template will be loaded and 
+        /// the parameters will be replace template's tags.
+        /// </summary>
+        /// <param name="userName">User name</param>
+        /// <param name="link">confirmation link</param>
+        /// <param name="body">email body.</param>
+        /// <returns>HTML with email data.</returns>
+        public static string CreateAccountConfirmationBody(string userName, string link, string body)
+        {
+            //This file load will be replaced for a cache system.
+            var templateData = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lib\\Email\\Templates\\AccountConfirmation.html"));
+            templateData = templateData.Replace("{ UserName }", userName);
+            templateData = templateData.Replace("{ Link }", link);
+            templateData = templateData.Replace("{ Body }", body);
+
+            return templateData;
         }
     }
 }
